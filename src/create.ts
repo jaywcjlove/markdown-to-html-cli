aaa51a8fd3002f7c7734025f5866f7a03b61406a
jaywcjlove <398188662@qq.com>
2021-09-26 23:47:26 +0800

    feat: Add octicon link.

--- src/create.ts
+++ src/create.ts
@@ -5,7 +5,9 @@ import rehypeDocument from 'rehype-document';
 // @ts-ignore
 import rehypePrism from '@mapbox/rehype-prism';
 import stringify from 'rehype-stringify';
+import slug from 'rehype-slug';
 import rehypeFormat from 'rehype-format';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 // @ts-ignore
 import rehypeWrap from 'rehype-wrap';
 import rehypeRewrite from 'rehype-rewrite';
@@ -14,6 +16,7 @@ import remarkGfm from 'remark-gfm';
 import remarkRehype from 'remark-rehype';
 import remarkParse from 'remark-parse';
 import { githubCorners } from './nodes/githubCorners';
+import { octiconLink } from './nodes/octiconLink';
 
 import { RunArgvs, MDToHTMLOptions } from './';
 
@@ -26,6 +29,8 @@ export function create(argvs: RunArgvs, options: MDToHTMLOptions = {}) {
     .use(remarkParse)
     .use(remarkGfm)
     .use(remarkRehype)
+    .use(slug)
+    .use(rehypeAutolinkHeadings)
     .use(rehypeDocument, {
       ...document,
       style: [cssStr.toString(), ...(Array.isArray(document.style) ? document.style : [document.style]) ],
@@ -36,6 +41,14 @@ export function create(argvs: RunArgvs, options: MDToHTMLOptions = {}) {
       const className = node.properties ? (node.properties as any).className || [] : [];
       if(node.type == 'element' && className.includes('wmde-markdown') && argvs['github-corners']) {
         node.children = [githubCorners({ href: argvs['github-corners'] }), ...(node.children as Array<any>)];
+      }
+
+      if (node.type === 'element' && /h(1|2|3|4|5|6)/.test((node as any).tagName) && Array.isArray(node.children)) {
+        const child = node.children && node.children[0];
+        if (child && child.properties) {
+          child.properties = { class: 'anchor', ...child.properties };
+          child.children = [octiconLink()];
+        }
       }
     })
     .use(rehypePrism)
