c8ff88b6881fe9b84eeb72f9872dd10a6df3d1d9
jaywcjlove <398188662@qq.com>
2022-03-07 18:52:40 +0800

    fix: Fix __dirname issue.

--- src/create.ts
+++ src/create.ts
@@ -30,10 +30,10 @@ import { MDToHTMLOptions } from './';
 
 // https://stackoverflow.com/questions/46745014/alternative-for-dirname-in-node-when-using-the-experimental-modules-flag
 // export const _dirname = dirname(fileURLToPath(import.meta.url));
-// const filename = fileURLToPath(import.meta.url);
-// const dirname = path.dirname(filename);
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 
-export const _dirname = __dirname;
+export const _dirname = dirname;
 export interface CreateOptions extends MDToHTMLOptions { }
 
 const script = `function copied(target, str) {
